dc70c712d3e69f2babd9fa51c6fd885c65ab3af7
Joao Lucas da Silva Ribeiro <joaolucas.jlsr@gmail.com>
2021-02-09 18:18:49 -0300

    Context Creation

---
+++ TARSTestJoaoLucas/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TARSTestJoaoLucas.Models;
+
+namespace TARSTestJoaoLucas.Context
+{
+    class AppDbContext : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
