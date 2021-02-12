4ad08bc4a5f2aad8dceee5d0a71bf7afbd4745d1
Joao Lucas da Silva Ribeiro <joaolucas.jlsr@gmail.com>
2021-02-11 22:44:44 -0300

    Removing https redirection

--- TARSTestJoaoLucas/Startup.cs
+++ TARSTestJoaoLucas/Startup.cs
@@ -48,7 +48,7 @@ namespace TARSTestJoaoLucas
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TARSTestJoaoLucas v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
